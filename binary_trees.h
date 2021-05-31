b288b61ed78741d8cd25836ca7ae6c55a2710444
Matias Pirez <67205865+Ripjawws@users.noreply.github.com>
2021-05-31 11:50:11 -0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -20,11 +21,20 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+
 typedef struct binary_tree_s bst_t;
+
+
+
 typedef struct binary_tree_s avl_t;
+
+
+
 typedef struct binary_tree_s heap_t;
-void binary_tree_print(const binary_tree_t *tree);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
@@ -37,4 +47,13 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int number_nodes(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int power(int num1, int num2);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif
