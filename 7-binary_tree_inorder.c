83d76e97f561f5e59cf1307d8abfd228227b6c3f
Matias Pirez <67205865+Ripjawws@users.noreply.github.com>
2021-05-31 11:46:26 -0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,27 +1,16 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - tree
- * @tree: parent
- * @func: func
- * Return: return
+*binary_tree_inorder - binary tree
+* @tree: tree
+* @func: func
+* Return: return
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	void (*function)(int) = func;
-
-	if (tree == NULL && func == NULL)
-	{
+	if (tree == NULL || func == NULL)
 		return;
-	}
-	if (tree->left != NULL)
-	{
-		binary_tree_inorder(tree->left, function);
-	}
-	function(tree->n);
-		if (tree->right != NULL)
-	{
-		binary_tree_inorder(tree->right, function);
-	}
 
-
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
