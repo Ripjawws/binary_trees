cb170e7fe33f5000bfbb4d21286fd29f2c14ebd7
Matias Pirez <67205865+Ripjawws@users.noreply.github.com>
2021-05-31 11:44:26 -0300

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,33 +1,36 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - tree
- * @parent: parent
- * @value: value
- * Return: return
+*binary_tree_insert_right - binary tree
+* @parent: parent
+* @value: value
+* Return: return
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *rightnode;
+	binary_tree_t *node;
+	binary_tree_t *right_node;
 
-	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
-
-	if (newnode == NULL)
+	if (!parent)
 	{
 		return (NULL);
 	}
 
-	newnode->n = value;
-	newnode->parent = parent;
-	newnode->right = NULL;
-	newnode->right = NULL;
-	rightnode = parent->right;
-	parent->right = newnode;
-
-	if (rightnode != NULL)
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
 	{
-		newnode->right = rightnode;
-		rightnode->parent = newnode;
+		return (NULL);
 	}
-	return (newnode);
+	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
+	right_node = parent->right;
+	parent->right = node;
+	node->parent = parent;
 
+	if (right_node != NULL)
+	{
+		node->right = right_node;
+		right_node->parent = node;
+	}
+	return (node);
 }
