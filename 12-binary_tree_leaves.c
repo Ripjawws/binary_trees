b0e2ff8d8896d920eb841486758cb2f9b69cb76a
Matias Pirez <67205865+Ripjawws@users.noreply.github.com>
2021-05-31 11:48:34 -0300

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,15 +1,27 @@
 #include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * binary_tree_leaves - tree
- * @tree: parent
- * Return: return
+*binary_tree_leaves - binary tree
+* @tree: tree
+* Return: return
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t right_count, left_count, total_count;
+
 	if (tree == NULL)
-	{
 		return (0);
+
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
 	}
-
-
+	else
+	{
+		left_count = binary_tree_leaves(tree->left);
+		right_count = binary_tree_leaves(tree->right);
+		total_count = left_count + right_count;
+		return (total_count);
+	}
 }
