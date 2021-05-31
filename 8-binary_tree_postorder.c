a3366d1a8486f02ad6e0b7142a4668ebe952a5d0
Matias Pirez <67205865+Ripjawws@users.noreply.github.com>
2021-05-31 11:47:15 -0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,26 +1,16 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - tree
- * @tree: parent
- * @func: func
- * Return: return
+*binary_tree_postorder - binary tree
+* @tree: tree
+* @func: func
+* Return: return
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	void (*function)(int) = func;
-
-	if (tree == NULL && func == NULL)
-	{
+	if (tree == NULL || func == NULL)
 		return;
-	}
-	if (tree->left != NULL)
-	{
-		binary_tree_postorder(tree->left, function);
-	}
-	if (tree->right != NULL)
-	{
-		binary_tree_postorder(tree->right, function);
-	}
-	function(tree->n);
 
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
